2213907b1bd0c5b828c5c32926da05e7eee0d7a2
Davidulik30 <61747978+Davidulik30@users.noreply.github.com>
2023-01-10 16:58:14 +0300

    Update csvreader.py

--- Checks 02 12 22/checks/csvreader.py
+++ Checks 02 12 22/checks/csvreader.py
@@ -2,11 +2,17 @@ import pandas as pd
 
 df = pd.read_csv('C:/Users/begku/Desktop/sell Data/2020-21.csv',sep=';', encoding='ANSI') 
 
-df=df.rename(columns={'Номенклатура': "name", 'Код':"idtov",
-       'Документ продажи.Номер':"iddoc", 'Количество':"count",'Цена':"price",'Сумма':"summa"})
+data=data.rename(columns={'Номенклатура': 'name', 'Код':'idtov',
+       'Документ продажи.Номер':'iddoc', 'Количество':'count','Цена':'price','Сумма':'summa'})
 
 data['return'] = ''
+data['kassa'] = ''
 
-print(df.keys())
+data['summa'] = data['summa'].replace(' ','', regex=True)
+data['summa'] = data['summa'].replace(',','.', regex=True).astype(float)
+#print(data.sort_values(by = ['summa'], ascending=False))
 
-#print(df["Цена"].to_list())
+tovs = data.groupby(['idtov']).sum() #общее кол-во товаров по id товара
+tovs['number_of_units_sold'] = data.groupby(['idtov']).size()
+tovs = tovs.sort_values(by = ['summa'], ascending=False)
+

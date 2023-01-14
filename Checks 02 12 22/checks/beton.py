f7f214618a9fcf8b6d35c316ec2edad88cfb5fac
David <begkuzarov@gmail.com>
2023-01-14 23:01:44 +0300

    14 01 23

--- Checks 02 12 22/checks/beton.py
+++ Checks 02 12 22/checks/beton.py
@@ -44,7 +44,7 @@ def checks_update():
     # data = pd.merge(dfTitles, data, on='iddoc')
     # data.head()
     # TEST >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    data = pd.read_csv('C:/Users/Admin/Desktop/sell Data/2020-21-edited.csv',sep=';', encoding='ANSI',low_memory=False) 
+    data = pd.read_csv('C:/Users/begku/Desktop/sell Data/2020-21-edited.csv',sep=',', encoding='utf-8',low_memory=False) 
     data['summa'] = data['summa'].replace(' ','', regex=True)
     data['summa'] = data['summa'].replace(',','.', regex=True).astype(float)
     data['price'] = data['price'].replace(' ','', regex=True)
--- Checks 02 12 22/checks/csvreader.py
+++ Checks 02 12 22/checks/csvreader.py
@@ -1,16 +1,20 @@
 import pandas as pd
 
-data = pd.read_csv('C:/Users/Admin/Desktop/sell Data/2020-21-edited.csv',sep=';', encoding='ANSI',low_memory=False) 
+data = pd.read_csv('C:/Users/begku/Desktop/sell Data/2020-21-edited.csv',sep=',', encoding='utf-8',low_memory=False) 
 
 data['summa'] = data['summa'].replace(' ','', regex=True)
 data['summa'] = data['summa'].replace(',','.', regex=True).astype(float)
 
 data['count'] = data['count'].replace(' ','', regex=True)
 data['count'] = data['count'].replace(',','.', regex=True).astype(float)
+
+data['price'] = data['price'].replace(' ','', regex=True)
+data['price'] = data['price'].replace(',','.', regex=True).astype(float)
 #print(data.sort_values(by = ['summa'], ascending=False))
 
-ch = data.groupby(['iddoc']).sum()
-print(data.keys())
-ch['count_uniq_good'] = data.groupby(['iddoc']).size()
+print(data['price'][1]*data['count'][1])
+print(data['summa'][1])
 
-print(ch.keys())
+ch = data.groupby(['iddoc']).sum()
+#print(data.keys())
+ch['count_uniq_good'] = data.groupby(['iddoc']).size()
